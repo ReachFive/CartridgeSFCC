8013b3456291de581236656a8a2b41f1d4904cf0
Tlestroubac-Reach5 <162098402+Tlestroubac-Reach5@users.noreply.github.com>
2024-08-29 08:15:18 +0200

    Update ReachFiveWebhooks.js

--- cartridges/int_reachfive/cartridge/controllers/ReachFiveWebhooks.js
+++ cartridges/int_reachfive/cartridge/controllers/ReachFiveWebhooks.js
@@ -30,4 +30,34 @@ server.post('DeleteUser', server.middleware.https, function(req, res, next) {
     next();
 });
 
+server.post('DeleteUser', server.middleware.https, function(req, res, next) {
+    var payload = req.httpParameterMap.getRequestBodyAsString();
+    LOGGER.warn("payload" + payload);
+    try {
+        var eventData = JSON.parse(payload);
+        var userId = eventData.user.id; 
+        
+        var customerProfile = salesforceInterface.findCustomerProfileByExternalID("ReachFive", userId);
+        
+        if (customerProfile) {
+            Transaction.wrap(function() {
+                LOGGER.warn("Utilisateur déconnecté (si possible)");
+            });
+            var result = salesforceInterface.deleteCustomerUsingOCAPI(customerProfile.getCustomer());
+            res.setStatusCode(200);
+        } else {
+            LOGGER.warn("No profile found");
+            res.setStatusCode(404);
+            res.json({ error: 'No profile found' });
+        }
+    } catch (e) {
+        LOGGER.error("Error when webhook is processing : " + e.toString());
+        res.setStatusCode(500);
+        res.json({ error: e.toString() });
+    }
+
+    next();
+});
+
 module.exports = server.exports();
+
